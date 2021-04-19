f393eb135f0cd6d7d4b3141e1c84f46a2ea1d95a
Raptors27 <daniespiirit@gmail.com>
2021-04-19 17:19:04 +0200

    comentario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'OrganizadorDeTareas2';
+  title = 'Mi primera app con Angular';
+  textoInput: string;
+  textoBoton: string;
+  textoTarea: string;
+  botonPulsado: boolean = false;
+  tareas: string[] = [];
+
+  mostrarTexto() {
+    this.textoBoton = `Has pulsado el boton y la variable contiene ${this.textoInput}`;
+    if (this.botonPulsado) {
+      this.botonPulsado = false;
+    } else { this.botonPulsado = true }
+  }
+
+  agregarLista(){
+      this.tareas.push(this.textoTarea);
+  }
+
 }
+
